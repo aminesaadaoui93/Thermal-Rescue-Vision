62cd5ab244cab0a7b7d75fce7a25ca6e005aa8d7
youssef <majdoubyoussef79@gmail.com>
2026-03-15 14:15:48 +0100

    better for handling multiprocessing

--- training_scripts/yolo_training_script.py
+++ training_scripts/yolo_training_script.py
@@ -44,13 +44,12 @@ def show_training_report(results):
 # --- Usage ---
 # results = model.train(...)
 # show_training_report(results)
-
-data_path = "C:/Users/medbe/OneDrive/Bureau/PFA2026/yolo_dataset"
-yaml_file = os.path.join(data_path, "dataset.yaml")
-model = YOLO("yolo26m.pt")
-# if you have i5 14th generation or more 6 workers is good for training,
-# if you have less than that you can set it to 0 or 2
-results = model.train(
-    data=yaml_file, epochs=100, batch=8, imgsz=640, device="cuda", workers=6
-)
-show_training_report(results)
+if __name__ == "__main__":
+    data_path = "C:/Users/medbe/OneDrive/Bureau/PFA2026/yolo_dataset"
+    yaml_file = os.path.join(data_path, "dataset.yaml")
+    model = YOLO("yolo26m.pt")
+    # if you have i5 14th generation or more 6 workers is good for training,# if you have less than that you can set it to 0 or 2
+    results = model.train(
+        data=yaml_file, epochs=100, batch=8, imgsz=640, device="cuda", workers=6
+    )
+    show_training_report(results)
