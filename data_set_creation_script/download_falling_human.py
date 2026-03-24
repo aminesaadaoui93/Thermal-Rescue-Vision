4d6a99729d1818a7bf208781329b3d4b90be4271
youssef <majdoubyoussef79@gmail.com>
2026-03-24 23:16:36 +0100

    checking to ensure downloading for the falling human dataset

--- data_set_creation_script/download_falling_human.py
+++ data_set_creation_script/download_falling_human.py
@@ -11,7 +11,7 @@ url = f"https://drive.google.com/uc?id={file_id}"
 output = os.path.join(path, "falling human.zip")
 gdown.download(url, output, quiet=False, fuzzy=True)
 if not os.path.exists(output):
-    raise Exception("Failed to download the dataset.")
+    raise Exception(f"Failed to download the dataset. from {url}")
 with zipfile.ZipFile(output, "r") as zip_ref:
     zip_ref.extractall(path)
 
