9eb99340c6a5c976d62e75671f61bcff87822543
youssef <majdoubyoussef79@gmail.com>
2026-03-24 23:16:01 +0100

    finilizing the falling human dataset downloading

---
+++ data_set_creation_script/download_falling_human.py
@@ -0,0 +1,18 @@
+import gdown
+import os
+import zipfile
+import shutil
+
+
+path = os.path.abspath("./")
+
+file_id = "1wMgfZTzfF7YF9A9kDTWaX2Zkpbmh5A19"
+url = f"https://drive.google.com/uc?id={file_id}"
+output = os.path.join(path, "falling human.zip")
+gdown.download(url, output, quiet=False, fuzzy=True)
+if not os.path.exists(output):
+    raise Exception("Failed to download the dataset.")
+with zipfile.ZipFile(output, "r") as zip_ref:
+    zip_ref.extractall(path)
+
+os.remove(output)
